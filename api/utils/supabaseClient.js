1581a90e50439ede30cdff782b66941af93757f1
unknown <202210867@gordoncollege.com>
2025-08-29 04:39:19 +0800

    initial commit recipe frontend

--- api/utils/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase = createClient(
-    "https://sfoyguywhcyqnxpvdexr.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmb3lndXl3aGN5cW54cHZkZXhyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODcxOTA3MSwiZXhwIjoyMDY0Mjk1MDcxfQ.k-bQQ1K_eb1USXU_DtOJGRy5aUSQCCkZw5ADIzTALGE"
-);
