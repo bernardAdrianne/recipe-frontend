66dd3c93df1bdf90d492548e08ab2fbd904b3b5e
unknown <202210867@gordoncollege.com>
2025-09-11 22:14:13 +0800

    deleted footer

--- src/lib/user/footer.svelte
+++ src/lib/user/footer.svelte
@@ -1,4 +1,4 @@
-<footer class="bg-[#228B22] text-white py-6 shadow-inner">
+<!-- <footer class="bg-[#228B22] text-white py-6 shadow-inner">
 	<div class="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
 		<p class="text-sm text-black">&copy; 2025 ReciPinoy. All rights reserved.</p>
 
@@ -7,4 +7,4 @@
 			<a href="/disclaimer" class="hover:underline text-black ">Disclaimers</a>
 		</div>
 	</div>
-</footer>
+</footer> -->
